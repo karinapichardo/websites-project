6c90a1056e18e2eb07f3dd219db8177b32286241
karinapichardo <pichardokarina21@yahoo.com>
2020-05-14 20:55:12 -0400

    more content

---
+++ scripts/message.js
@@ -0,0 +1,14 @@
+function sendMessage() {
+    var sendButton = document.querySelector('#sendButton');
+    var messageIn = document.querySelector('#messageIn');
+    var messageOut = document.querySelector('#messageOut');
+
+    sendButton.addEventListener('click', function () {
+        var content = messageIn.value;
+        messageOut.innerHTML = content;
+        //        messageIn.value = '';
+
+    })
+}
+
+setInterval(sendMessage);
